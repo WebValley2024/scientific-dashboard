d9b588b4d7e52f525227010622ca16f22932d78d
John Anand <97368418+bigtech-nerd@users.noreply.github.com>
2024-07-01 14:58:59 -0400

    Update scm.py

--- webappfiles/scm.py
+++ webappfiles/scm.py
@@ -130,10 +130,10 @@ from reducefreq import reduce_frequency
 def plot_SCM(path):
     f = xr.open_dataset(path, engine='h5netcdf', phony_dims='sort')
     # f = xr.open_dataset(path, phony_dims='sort')
-    X_Waveform = f['A231_W'][...]
-    Y_Waveform = f['A232_W'][...]
-    Z_Waveform = f['A233_W'][...]
-    verse_time = f['VERSE_TIME'][...].values.flatten()
+    X_Waveform = f['A231_W'][300:]  # Skip the first 300 points
+    Y_Waveform = f['A232_W'][300:]  # Skip the first 300 points
+    Z_Waveform = f['A233_W'][300:]  # Skip the first 300 points
+    verse_time = f['VERSE_TIME'][300:].values.flatten()  # Skip the first 300 points
     X_Power_spectrum = f['A231_P'][...]
     Y_Power_spectrum = f['A232_P'][...]
     Z_Power_spectrum = f['A233_P'][...]
@@ -146,11 +146,11 @@ def plot_SCM(path):
     magnitude = np.sqrt(X_Waveform**2 + Y_Waveform**2 + Z_Waveform**2)
     polar_angle = np.arccos(Z_Waveform / magnitude)  # theta
     azimuthal_angle = np.arctan2(Y_Waveform, X_Waveform)  # phi
- 
+
     # Convert angles to degrees
     polar_angle = np.degrees(polar_angle)
     azimuthal_angle = np.degrees(azimuthal_angle)
- 
+
     reduced_freq = 100  # Specify the desired frequency here
     X_Waveform = reduce_frequency(X_Waveform, reduced_freq)
     Y_Waveform = reduce_frequency(Y_Waveform, reduced_freq)
@@ -158,18 +158,16 @@ def plot_SCM(path):
     magnitude = reduce_frequency(magnitude, reduced_freq)
     polar_angle = reduce_frequency(polar_angle, reduced_freq)
     azimuthal_angle = reduce_frequency(azimuthal_angle, reduced_freq)
- 
- 
- 
+
     X_Waveform = X_Waveform.values.flatten()
     Y_Waveform = Y_Waveform.values.flatten()
     Z_Waveform = Z_Waveform.values.flatten()
     magnitude = magnitude.values.flatten()
     polar_angle = polar_angle.values.flatten()
     azimuthal_angle = azimuthal_angle.values.flatten()
- 
+
     len_time = len(verse_time)
- 
+
     # Ensure vers_extend has the same length as the waveforms
     vers_extend = np.concatenate([np.linspace(verse_time[i], verse_time[i+1], reduced_freq, endpoint=False) for i in range(len_time-1)])
     vers_extend = np.concatenate([vers_extend, np.linspace(verse_time[-2], verse_time[-1], reduced_freq)])
@@ -179,19 +177,19 @@ def plot_SCM(path):
     fig1.add_trace(go.Scatter(x=vers_extend, y=Y_Waveform, mode='lines', name='Y Waveform'))
     fig1.add_trace(go.Scatter(x=vers_extend, y=Z_Waveform, mode='lines', name='Z Waveform'))
     fig1.add_trace(go.Scatter(x=vers_extend, y=magnitude, mode='lines', name='Vector sum'))
- 
+
     fig1.update_layout(
         title="X, Y, Z Waveforms and the Vector Sum",
         xaxis_title="Time (ms)",
         yaxis_title=waveform_unit,
         legend=dict(x=1, y=0.5)
     )
- 
+
     # Second figure for polar and azimuthal angles
     fig2 = go.Figure()
     fig2.add_trace(go.Scatter(x=vers_extend, y=polar_angle, mode='lines', name='Polar Angle', yaxis='y2'))
     fig2.add_trace(go.Scatter(x=vers_extend, y=azimuthal_angle, mode='lines', name='Azimuthal Angle', yaxis='y3'))
- 
+
     fig2.update_layout(
         title="Polar and Azimuthal Angles",
         xaxis_title="Time (ms)",
@@ -208,7 +206,7 @@ def plot_SCM(path):
         ),
         legend=dict(x=1.08, y=0.54)
     )
- 
+
     # Third figure for power spectra
     power_spectra = {'X': X_Power_spectrum, 'Y': Y_Power_spectrum, 'Z': Z_Power_spectrum}
     for axis, spectrum in power_spectra.items():
@@ -228,15 +226,15 @@ def plot_SCM(path):
         )
         st.plotly_chart(fig)
         # fig.show()
- 
+
     # Display the first two figures
     st.plotly_chart(fig1)
     st.plotly_chart(fig2)
     # fig1.show()
     # fig2.show()
- 
- 
- 
+
+
+
 def plot_SCM_on_map(data, latitude, longitude):
     try:
         freq = data.shape[1]
@@ -244,30 +242,30 @@ def plot_SCM_on_map(data, latitude, longitude):
         freq = 1
     print(data.shape)
     st.write(data.shape)
- 
+
     measure = data.values.flatten()
- 
+
     lon = longitude.values.flatten()
     lat = latitude.values.flatten()
- 
+
     measure_log = np.log10(measure + 1)
    
     length_coord = len(lon)
     length_measure = len(measure)
- 
+
     lon_extend = np.concatenate([np.linspace(lon[i], lon[i+1], freq, endpoint=False) for i in range(length_coord-1)])
     lon_extend = np.concatenate([lon_extend, np.linspace(lon[-2], lon[-1], freq)])
- 
+
     lat_extend = np.concatenate([np.linspace(lat[i], lat[i+1], freq, endpoint=False) for i in range(length_coord-1)])
     lat_extend = np.concatenate([lat_extend, np.linspace(lat[-2], lat[-1], freq)])
- 
+
     # Create a scatter plot on a map using Plotly
     fig = go.Figure()
- 
+
     # Load world map using geopandas
     world = gpd.read_file('ne_110m_admin_0_countries.shp')
     # world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
- 
+
     # Plot world map as background
     fig.add_trace(go.Choropleth(
         geojson=world.__geo_interface__,
@@ -277,6 +275,21 @@ def plot_SCM_on_map(data, latitude, longitude):
         hoverinfo='none',
         showscale=False
     ))
+
+def scmplot(file_path):
+    f = xr.open_dataset(file_path, engine='h5netcdf', phony_dims='sort')
+    latitude = f['MAG_LAT'][...]
+    longitude = f['MAG_LON'][...]
+    X_Waveform = f['A231_W'][300:]  # Skip the first 300 points
+    Y_Waveform = f['A232_W'][300:]  # Skip the first 300 points
+    Z_Waveform = f['A233_W'][300:]  # Skip the first 300 points
+    X_waveform = reduce_frequency(X_Waveform, 1)
+    Y_waveform = reduce_frequency(Y_Waveform, 1)
+    Z_waveform = reduce_frequency(Z_Waveform, 1)
+    magnitude=  np.sqrt(X_waveform**2 + Y_waveform**2 + Z_waveform**2)
+    plot_SCM_on_map(magnitude, latitude, longitude)
+    plot_SCM(file_path)
+
  
     # Scatter plot for data points
     # fig.add_trace(go.Scattergeo(
@@ -363,19 +376,4 @@ def plot_SCM_on_map(data, latitude, longitude):
 # plot_SCM_on_map(magnitude, latitude, longitude)
 # plot_SCM(path)
  
- 
-def scmplot(file_path):
-    f = xr.open_dataset(file_path, engine='h5netcdf', phony_dims='sort')
-    latitude = f['MAG_LAT'][...]
-    longitude = f['MAG_LON'][...]
-    X_Waveform = f['A231_W'][...]
-    Y_Waveform = f['A232_W'][...]
-    Z_Waveform = f['A233_W'][...]
-    X_waveform = reduce_frequency(X_Waveform, 1)
-    Y_waveform = reduce_frequency(Y_Waveform, 1)
-    Z_waveform = reduce_frequency(Z_Waveform, 1)
-    magnitude=  np.sqrt(X_waveform**2 + Y_waveform**2 + Z_waveform**2)
-    plot_SCM_on_map(magnitude, latitude, longitude)
-    plot_SCM(file_path)
-    # plot_EFD_on_map(file_path)
  
