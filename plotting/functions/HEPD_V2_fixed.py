1284b3dc0f33eecfc7aae0758cb04ee45a94eec2
John Anand <97368418+bigtech-nerd@users.noreply.github.com>
2024-07-03 06:52:30 -0400

    Update HEPD_V2_fixed.py

--- plotting/functions/HEPD_V2_fixed.py
+++ plotting/functions/HEPD_V2_fixed.py
@@ -7,27 +7,27 @@ from plotly.subplots import make_subplots
 import xarray as xr
 import geopandas as gpd
 
-def plot_proton_electron_count_utc(path):
-    st.write("herhehre")
-    if not path:
-        return
-    f = xr.open_zarr(path)
+@st.cache(allow_output_mutation=True)
+def load_data(path):
+    return xr.open_zarr(path)
 
-    time = f.UTCTime
+def convert_to_utc_time(date_strings):
+    utc_times = pd.to_datetime(date_strings, format="%Y%m%d%H%M%S%f", utc=True)
+    return utc_times
+
+def plot_proton_electron_count_utc(f):
+    st.write("Loading data for Proton and Electron Counts...")
+    time = f['UTCTime']
     try:
-        data = f.HEPD_ele_counts
-        data2 = f.HEPD_pro_counts
-    except:
-        data = f.Count_Electron
-        data2 = f.Count_Proton
-
-    def convert_to_utc_time(date_strings):
-        utc_times = pd.to_datetime(date_strings, format="%Y%m%d%H%M%S%f", utc=True)
-        return utc_times
+        data = f['HEPD_ele_counts']
+        data2 = f['HEPD_pro_counts']
+    except KeyError:
+        data = f['Count_Electron']
+        data2 = f['Count_Proton']
 
     try:
         freq = data.shape[1]
-    except:
+    except IndexError:
         freq = 1
 
     data = data.values[1:].flatten()
@@ -93,30 +93,21 @@ def plot_spectrogram(data, time, pitch_table, title, yaxis_title):
     )
     st.plotly_chart(fig)
 
-def plot_electron_energy_utc(path):
-    f = xr.open_dataset(path, engine='h5netcdf', phony_dims='sort')
-    plot_spectrogram(f.HEPD_ele_energy_pitch, f.UTCTime, f.HEPD_ele_energy_table, 
-                     "Electron Energy over UTC Time", f.HEPD_ele_energy_table.Units)
+def plot_energy_utc(f, energy_type, energy_pitch, energy_table):
+    data = f[energy_type]
+    time = f['UTCTime']
+    plot_spectrogram(data, time, energy_pitch, f"{energy_type} Energy over UTC Time", energy_table.Units)
 
-def plot_proton_energy_utc(path):
-    f = xr.open_dataset(path, engine='h5netcdf', phony_dims='sort')
-    plot_spectrogram(f.HEPD_pro_energy_pitch, f.UTCTime, f.HEPD_pro_energy_table, 
-                     "Proton Energy over UTC Time", f.HEPD_pro_energy_table.Units)
+def plot_pitch_utc(f, pitch_type, pitch_table):
+    data = f[pitch_type]
+    time = f['UTCTime']
+    plot_spectrogram(data, time, pitch_table, f"{pitch_type} over UTC Time", "Pitch Angle (degrees)")
 
-def plot_electron_pitch_utc(path):
-    f = xr.open_dataset(path, engine='h5netcdf', phony_dims='sort')
-    plot_spectrogram(f.HEPD_ele_pitch_table, f.UTCTime, f.HEPD_ele_pitch_table, 
-                     "Electron Pitch over UTC Time", "Pitch Angle (degrees)")
+def plot_counts_on_map(f, counts_type):
+    st.write(f"Loading data for {counts_type} Counts on Map...")
+    data = f[counts_type]
+    LonLat = f['LonLat']
 
-def plot_proton_pitch_utc(path):
-    f = xr.open_dataset(path, engine='h5netcdf', phony_dims='sort')
-    plot_spectrogram(f.HEPD_pro_pitch_table, f.UTCTime, f.HEPD_pro_pitch_table, 
-                     "Proton Pitch over UTC Time", "Pitch Angle (degrees)")
-
-def plot_electrons_counts_on_map(path):
-    f = xr.open_zarr(path)
-    data = f.HEPD_ele_counts
-    LonLat = f.LonLat
     if LonLat.ndim == 3:
         longitude = LonLat[0, 0, :]
         latitude = LonLat[0, 0, :]
@@ -125,6 +116,7 @@ def plot_electrons_counts_on_map(path):
         latitude = LonLat[:, 1]
     else:
         raise ValueError("Unexpected LonLat dimensions")
+
     try:
         freq = data.shape[1]
     except IndexError:
@@ -188,95 +180,7 @@ def plot_electrons_counts_on_map(path):
     )
 
     fig.update_layout(
-        title="Electrons counts during the orbit",
-        geo=dict(
-            showframe=False,
-            showcoastlines=False,
-            projection_type='natural earth'
-        ),
-        geo_scope='world',
-        height=600,
-        margin={"r":0,"t":30,"l":0,"b":0},
-        showlegend=False
-    )
-    st.plotly_chart(fig)
-
-def plot_protons_counts_on_map(path):
-    f = xr.open_zarr(path)
-    data = f.HEPD_pro_counts
-    LonLat = f.LonLat
-    if LonLat.ndim == 3:
-        longitude = LonLat[0, 0, :]
-        latitude = LonLat[0, 0, :]
-    elif LonLat.ndim == 2:
-        longitude = LonLat[:, 0]
-        latitude = LonLat[:, 1]
-    else:
-        raise ValueError("Unexpected LonLat dimensions")
-    try:
-        freq = data.shape[1]
-    except IndexError:
-        freq = 1
-
-    measure = data.values.flatten()
-
-    lon = longitude.values.flatten()
-    lat = latitude.values.flatten()
-
-    length_coord = len(lon)
-    length_measure = len(measure)
-
-    lon_extend = np.concatenate([np.linspace(lon[i], lon[i+1], freq, endpoint=False) for i in range(length_coord-1)])
-    lon_extend = np.concatenate([lon_extend, np.linspace(lon[-2], lon[-1], freq)])
-
-    lat_extend = np.concatenate([np.linspace(lat[i], lat[i+1], freq, endpoint=False) for i in range(length_coord-1)])
-    lat_extend = np.concatenate([lat_extend, np.linspace(lat[-2], lat[-1], freq)])
-
-    fig = go.Figure()
-    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
-
-    fig.add_trace(go.Choropleth(
-        geojson=world.__geo_interface__,
-        locations=world.index,
-        z=np.zeros(len(world)),
-        colorscale=[[0, 'lightgrey'], [1, 'lightgrey']],
-        hoverinfo='none',
-        showscale=False
-    ))
-
-    fig.add_trace(go.Scattergeo(
-        lon=lon_extend,
-        lat=lat_extend,
-        mode='markers',
-        marker=dict(
-            size=10,
-            color=measure,
-            colorscale='Viridis',
-            colorbar=dict(title="Counts"),
-            opacity=0.8,
-            colorbar_thickness=20,
-            colorbar_x=0.85,
-            colorbar_y=0.5,
-            colorbar_bgcolor='rgba(255,255,255,0.5)'
-        ),
-        name="Field magnitude"
-    ))
-
-    fig.update_geos(
-        projection_type="natural earth",
-        landcolor="lightgrey",
-        oceancolor="lightblue",
-        showland=True,
-        showocean=True,
-        showcountries=True,
-        showcoastlines=True,
-        showframe=False,
-        coastlinewidth=0.5,
-        coastlinecolor="white"
-    )
-
-    fig.update_layout(
-        title="Protons counts during the orbit",
+        title=f"{counts_type} Counts during the orbit",
         geo=dict(
             showframe=False,
             showcoastlines=False,
@@ -290,22 +194,24 @@ def plot_protons_counts_on_map(path):
     st.plotly_chart(fig)
 
 def plot_HEPPD(path):
-    plot_proton_electron_count_utc(path)
+    st.header("Proton and Electron Counts")
+    f = load_data(path)
+    plot_proton_electron_count_utc(f)
     
     st.header("Electron Energy Spectrum")
-    plot_electron_energy_utc(path)
+    plot_energy_utc(f, 'HEPD_ele_energy_pitch', f['HEPD_ele_energy_table'])
     
     st.header("Proton Energy Spectrum")
-    plot_proton_energy_utc(path)
+    plot_energy_utc(f, 'HEPD_pro_energy_pitch', f['HEPD_pro_energy_table'])
     
     st.header("Electron Pitch Angle")
-    plot_electron_pitch_utc(path)
+    plot_pitch_utc(f, 'HEPD_ele_pitch_table')
     
     st.header("Proton Pitch Angle")
-    plot_proton_pitch_utc(path)
+    plot_pitch_utc(f, 'HEPD_pro_pitch_table')
     
     st.header("Electron Counts on Map")
-    plot_electrons_counts_on_map(path)
+    plot_counts_on_map(f, 'HEPD_ele_counts')
     
     st.header("Proton Counts on Map")
-    plot_protons_counts_on_map(path)
+    plot_counts_on_map(f, 'HEPD_pro_counts')
